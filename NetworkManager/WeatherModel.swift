88614c91cbad9ba9c78786b1d5d4cfdad3233564
nbastreetgame <popov.andrey.s@icloud.com>
2025-01-17 23:42:37 +0700

    изменения

--- NetworkManager/WeatherModel.swift
+++ NetworkManager/WeatherModel.swift
@@ -1,125 +1,97 @@
-//
-//  convertjson.swift
-//  WeatherStore
-//
-//  Created by Андрей Попов on 16.01.2025.
-//
-
-
 import Foundation
 
-// MARK: - Welcome
+// MARK: - WeatherModel
 struct WeatherModel: Codable {
-    let cod: String
-    let message, cnt: Int
-    let list: [List]
+    let list: [WeatherEntry]
     let city: City
 }
-
 // MARK: - City
 struct City: Codable {
-    let id: Int
     let name: String
-    let coord: Coord
-    let country: String
-    let population, timezone, sunrise, sunset: Int
 }
 
-// MARK: - Coord
-struct Coord: Codable {
-    let lat: Int
-    let lon: Double
-}
-
-// MARK: - List
-struct List: Codable {
-    let dt: Int
+// MARK: - WeatherEntry
+struct WeatherEntry: Codable {
     let main: MainClass
     let weather: [Weather]
-    let clouds: Clouds
-    let wind: Wind
-    let visibility: Int
-    let pop: Double
-    let sys: Sys
-    let dtTxt: String
-    let rain: Rain?
-
-    enum CodingKeys: String, CodingKey {
-        case dt, main, weather, clouds, wind, visibility, pop, sys
-        case dtTxt = "dt_txt"
-        case rain
+    
+    // MARK: - MainClass
+    struct MainClass: Codable {
+        let temp: Double // Temperature in Kelvin
+    }
+    
+    // MARK: - Weather
+    struct Weather: Codable {
+        let main: WeatherCondition // Weather condition
+    }
+    
+    // Enum for weather conditions
+    enum WeatherCondition: String, Codable {
+        case clear = "Clear"
+        case clouds = "Clouds"
+        case rain = "Rain"
     }
 }
 
-// MARK: - Clouds
-struct Clouds: Codable {
-    let all: Int
-}
 
-// MARK: - MainClass
-struct MainClass: Codable {
-    let temp, feelsLike, tempMin, tempMax: Double
-    let pressure, seaLevel, grndLevel, humidity: Int
-    let tempKf: Double
-
-    enum CodingKeys: String, CodingKey {
-        case temp
-        case feelsLike = "feels_like"
-        case tempMin = "temp_min"
-        case tempMax = "temp_max"
-        case pressure
-        case seaLevel = "sea_level"
-        case grndLevel = "grnd_level"
-        case humidity
-        case tempKf = "temp_kf"
+// Function to print weather data
+func printWeatherData(_ weatherModel: WeatherModel) {
+    print("City: \(weatherModel.city.name)")
+    print("Weather forecast:\n")
+    
+    
+    // Function for testing JSON decoding
+    func testDecodingWeatherModel() {
+        // Example JSON data
+        let jsonData = """
+    {
+      "city": {
+        "name": "San Francisco"
+      },
+      "list": [
+        {
+          "main": {
+            "temp": 282.12
+          },
+          "weather": [
+            {
+              "main": "Clouds"
+            }
+          ]
+        },
+        {
+          "main": {
+            "temp": 283.81
+          },
+          "weather": [
+            {
+              "main": "Clouds"
+            }
+          ]
+        },
+        {
+          "main": {
+            "temp": 284.83
+          },
+          "weather": [
+            {
+              "main": "Clear"
+            }
+          ]
+        }
+      ]
     }
-}
-
-// MARK: - Rain
-struct Rain: Codable {
-    let the3H: Double
-
-    enum CodingKeys: String, CodingKey {
-        case the3H = "3h"
+    """.data(using: .utf8)!
+        
+        do {
+            // Decode JSON data into WeatherModel
+            let weatherData = try JSONDecoder().decode(WeatherModel.self, from: jsonData)
+            printWeatherData(weatherData) // Print weather information
+        } catch {
+            print("Decoding error: \(error.localizedDescription)") // Error handling
+        }
     }
+    
 }
 
-// MARK: - Sys
-struct Sys: Codable {
-    let pod: Pod
-}
-
-enum Pod: String, Codable {
-    case d = "d"
-    case n = "n"
-}
-
-// MARK: - Weather
-struct Weather: Codable {
-    let id: Int
-    let main: MainEnum
-    let description: Description
-    let icon: String
-}
-
-enum Description: String, Codable {
-    case brokenClouds = "broken clouds"
-    case clearSky = "clear sky"
-    case fewClouds = "few clouds"
-    case lightRain = "light rain"
-    case overcastClouds = "overcast clouds"
-    case scatteredClouds = "scattered clouds"
-}
-
-enum MainEnum: String, Codable {
-    case clear = "Clear"
-    case clouds = "Clouds"
-    case rain = "Rain"
-}
-
-// MARK: - Wind
-struct Wind: Codable {
-    let speed: Double
-    let deg: Int
-    let gust: Double
-}
+    
